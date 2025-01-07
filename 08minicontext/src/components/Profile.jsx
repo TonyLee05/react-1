b4990d67968b88e6a5165c338d72ed2874da6eff
TonyLee05 <leet3891@gmail.com>
2025-01-07 16:04:54 +0530

    Added projects

---
+++ 08minicontext/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <h1>Not logged In</h1>;
+  }
+  return (
+  <h1>
+    Profile: {user.username} 
+  </h1>
+  )
+}
+
+export default Profile;
